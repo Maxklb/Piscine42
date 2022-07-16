064ea295f9b1bb1c767850d46eaeef05c46c1918
Maxklb <themaxklb@gmail.com>
2022-07-16 21:15:42 +0200

    Add ex03

--- C03/ex03/ft_strncat.c
+++ C03/ex03/ft_strncat.c
@@ -6,8 +6,26 @@
 /*   By: maxklb <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 21:08:03 by maxklb            #+#    #+#             */
-/*   Updated: 2022/07/16 21:08:28 by maxklb           ###   ########.fr       */
+/*   Updated: 2022/07/16 21:15:21 by maxklb           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 
+char *ft_strcat(char *dest, char *src)
+{
+	char	*x;
+
+	x = dest;
+	while (*x != '\0')
+		x++;
+	while (*src != '\0' && nb > 0)
+	{
+		*x = *(unsigned char *)src;
+		x++;
+		src++;
+		nb --;
+	}
+	*x = '\0';
+	return (dest);
+}
