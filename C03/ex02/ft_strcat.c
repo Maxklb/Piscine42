24d92bd1161609a81b0d0afbe4378ed9f04ef441
Maxklb <themaxklb@gmail.com>
2022-07-16 21:08:50 +0200

    Add ex02 to C03

--- C03/ex02/ft_strcat.c
+++ C03/ex02/ft_strcat.c
@@ -6,7 +6,7 @@
 /*   By: mkoch-le <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/16 14:01:52 by mkoch-le          #+#    #+#             */
-/*   Updated: 2022/07/16 14:02:57 by mkoch-le         ###   ########.fr       */
+/*   Updated: 2022/07/16 18:32:31 by maxklb           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,16 @@
 char *ft_strcat(char *dest, char *src)
 {
 	char	*x;
+
+	x = dest;
+	while (*x != '\0')
+		x++;
+	while (*src != '\0')
+	{
+		*x = *(unsigned char *)src;
+		x++;
+		src++;
+	}
+	*x = '\0';
+	return (dest);
+}
