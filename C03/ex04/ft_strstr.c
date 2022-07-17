9e097f5963dfdd9a748218b1d156201e48df63dd
Maximilien KOCH LE BRETON <themaxklb@gmail.com>
2022-07-17 20:37:34 +0200

    Commit for home

--- C03/ex04/ft_strstr.c
+++ C03/ex04/ft_strstr.c
@@ -6,7 +6,7 @@
 /*   By: mkoch-le <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 14:02:37 by mkoch-le          #+#    #+#             */
-/*   Updated: 2022/07/17 20:27:17 by mkoch-le         ###   ########.fr       */
+/*   Updated: 2022/07/17 20:30:37 by mkoch-le         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,8 @@ char	*ft_strstr(char *str, char *to_find)
 	{
 		if (*str == *to_find)
 		{
-			
+			if (!to_find[index])
+				return (str);	
 		}
 		str++
 	}
