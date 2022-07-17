c9b0fa912da390576e2e4e4ad2dbe8a121affaa2
Maximilien KOCH LE BRETON <themaxklb@gmail.com>
2022-07-17 12:00:10 +0200

    Add Atoi

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mkoch-le <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 11:21:31 by mkoch-le          #+#    #+#             */
-/*   Updated: 2022/07/17 11:26:38 by mkoch-le         ###   ########.fr       */
+/*   Updated: 2022/07/17 11:59:21 by mkoch-le         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,21 @@
 
 int	ft_atoi(char *str)
 {
-	
+	int	result;
+	int	sign;
+
+	result = 0;
+	sign = 1;
+	while (*str == 32 || (*str >= 9 && *str <= 13))
+		str++;
+	if (*str == '-')
+		sign += -1;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str >= '0' && *str <= '9')
+	{
+		result = result * 10 + *str - '0';
+		str++;
+	}
+	return (result * sign);
+}
