a2a4a82a3d6f372af426083424bcd8d7e77419b7
Maximilien KOCH LE BRETON <themaxklb@gmail.com>
2022-07-17 15:14:02 +0200

    Add new ex to C06

--- C06/ex01/ft_print_params.c
+++ C06/ex01/ft_print_params.c
@@ -6,10 +6,38 @@
 /*   By: mkoch-le <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 14:59:14 by mkoch-le          #+#    #+#             */
-/*   Updated: 2022/07/17 15:01:51 by mkoch-le         ###   ########.fr       */
+/*   Updated: 2022/07/17 15:10:13 by mkoch-le         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	ft_putchar(char *str)
+{
+	int	index;
 
+	index = 0;
+	while (str[index] != '\0')
+	{
+		write (1, &str[index], 1);
+		index++;
+	}
+}
+
+void	ft_putchar2(char c)
+{
+	write (1, &c, 1);
+}
+
+int main (int argc, char *argv[])
+{
+	int	x;
+
+	x = 1;
+	while (x < argc)
+	{
+		ft_putchar(argv[x]);
+		x++;
+		ft_putchar2('\n');
+	}
+}
