320e2f449ed7ba09de3b60cd53073ad7fb3297e2
Maximilien KOCH LE BRETON <themaxklb@gmail.com>
2022-07-17 11:27:04 +0200

    test

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -1,24 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mkoch-le <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/13 15:58:50 by mkoch-le          #+#    #+#             */
-/*   Updated: 2022/07/17 11:19:49 by mkoch-le         ###   ########.fr       */
+/*   Created: 2022/07/17 11:21:31 by mkoch-le          #+#    #+#             */
+/*   Updated: 2022/07/17 11:26:38 by mkoch-le         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-char	*ft_strcapitalize(char *str)
+int	ft_atoi(char *str)
 {
-	int	index;
-
-	index = 0;
-	if (str[index] >= 'A' && str[index] <= 'Z')
-		str[index] += 32;
-	while (str[index] != '\0')
-	{
-		
+	
