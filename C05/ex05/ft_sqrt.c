f4348d4e7f70c541fd1a42a288a277c677ddab7e
Maximilien KOCH LE BRETON <themaxklb@gmail.com>
2022-07-20 18:55:36 +0200

    YES

--- C05/ex05/ft_sqrt.c
+++ C05/ex05/ft_sqrt.c
@@ -6,7 +6,7 @@
 /*   By: mkoch-le <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 17:41:07 by mkoch-le          #+#    #+#             */
-/*   Updated: 2022/07/20 17:50:05 by mkoch-le         ###   ########.fr       */
+/*   Updated: 2022/07/20 18:54:42 by mkoch-le         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,16 @@
 
 int	ft_sqrt(int nb)
 {
+	int	n;
+
+	n = 1;
 	if (nb < 1)
 		return (0);
-	
-
-
-
-
-}
-
-int	main(void)
-{
-	printf("%d",ft_sqrt(5));
+	while (n * n < nb)
+	{
+		n++;
+	}
+	if (n * n > nb)
+		return (0);
+	return (n);
 }
