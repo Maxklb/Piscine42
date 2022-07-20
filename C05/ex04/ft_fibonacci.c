58770ed2c9c3713907b742a5ed1df7937b4639ec
Maximilien KOCH LE BRETON <themaxklb@gmail.com>
2022-07-20 17:40:29 +0200

    Add new ex

--- C05/ex04/ft_fibonacci.c
+++ C05/ex04/ft_fibonacci.c
@@ -6,10 +6,15 @@
 /*   By: mkoch-le <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 15:31:02 by mkoch-le          #+#    #+#             */
-/*   Updated: 2022/07/20 15:31:43 by mkoch-le         ###   ########.fr       */
+/*   Updated: 2022/07/20 17:37:06 by mkoch-le         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_fibonacci(int index)
 {
-	
+	if (index < 0)
+		return (-1);
+	if (index < 2)
+		return (index);
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
