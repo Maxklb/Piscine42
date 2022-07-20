1dfd99e502d774908fe9dc0e3c03a984ee553113
Maximilien KOCH LE BRETON <themaxklb@gmail.com>
2022-07-20 11:27:55 +0200

    Add ex02

--- C05/ex02/ft_iterative_power.c
+++ C05/ex02/ft_iterative_power.c
@@ -6,7 +6,7 @@
 /*   By: mkoch-le <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 19:03:53 by mkoch-le          #+#    #+#             */
-/*   Updated: 2022/07/19 19:21:05 by mkoch-le         ###   ########.fr       */
+/*   Updated: 2022/07/20 11:23:38 by mkoch-le         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,22 @@
 
 int	ft_iterative_power(int nb, int power)
 {
-	
+	int	n;
+	int	i;
+
+	n = 1;
+	i = nb;
+	if (power < 0)
+		return (0);
+	else if ((nb == 0 && power == 0) || power == 0)
+		return (1);
+	else if (power > 0)
+	{
+		while (n < power)
+		{
+			nb = nb * i;
+			n++;
+		}
+		return (nb);
+	}
+}
