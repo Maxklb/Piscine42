e87130fe536c0249eb34a3a4dc43d985984b1221
Maximilien KOCH LE BRETON <themaxklb@gmail.com>
2022-07-21 14:06:49 +0200

    Last commit

--- C05/ex06/ft_is_prime.c
+++ C05/ex06/ft_is_prime.c
@@ -6,20 +6,20 @@
 /*   By: mkoch-le <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/20 19:11:05 by mkoch-le          #+#    #+#             */
-/*   Updated: 2022/07/21 13:36:38 by mkoch-le         ###   ########.fr       */
+/*   Updated: 2022/07/21 14:06:36 by mkoch-le         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_is_prime(int nb)
 {
-	int	i;
+	unsigned int	i;
 
 	i = 2;
 	if (nb < 2)
 		return (0);
 	if (nb >= 2)
 	{
-		while (i < 10)
+		while (i *i < nb)
 		{
 			if (nb % i == 0)
 				return (0);
