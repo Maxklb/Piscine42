5f9efcdb0d72ee0ad0fce90a385677111b68c2b2
Maximilien KOCH LE BRETON <themaxklb@gmail.com>
2022-07-22 13:53:17 +0200

    Fix atoi

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: mkoch-le <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 11:21:31 by mkoch-le          #+#    #+#             */
-/*   Updated: 2022/07/21 17:49:13 by mkoch-le         ###   ########.fr       */
+/*   Updated: 2022/07/22 13:27:08 by mkoch-le         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,12 @@ int	ft_atoi(char *str)
 	sign = 1;
 	while (*str == 32 || (*str >= 9 && *str <= 13))
 		str++;
-	if (*str == '-')
-		sign *= -1;
 	while (*str == '-' || *str == '+')
+	{	
+		if (*str == '-')
+			sign *= -1;
 		str++;
+	}
 	while (*str >= '0' && *str <= '9')
 	{
 		result = result * 10 + *str - '0';
--- C04/ex04/ft_putnbr_base.c
+++ C04/ex04/ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: mkoch-le <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 12:04:20 by mkoch-le          #+#    #+#             */
-/*   Updated: 2022/07/21 17:55:01 by mkoch-le         ###   ########.fr       */
+/*   Updated: 2022/07/22 13:30:43 by mkoch-le         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,4 +81,26 @@ void	ft_putnbr_base(int nbr, char *base)
 	else
 		number = nbr;
 	ft_putnbr(number, base);
+}
+
+#include <unistd.h>
+
+void	ft_putnbr_base(int nbr, char *base);
+
+int		main(void)
+{
+	write(1, "42:", 3);
+	ft_putnbr_base(42, "0123456789");
+	write(1, "\n2a:", 4);
+	ft_putnbr_base(42, "0123456789abcdef");
+	write(1, "\n-2a:", 5);
+	ft_putnbr_base(-42, "0123456789abcdef");
+	write(1, "\n:", 2);
+	ft_putnbr_base(42, "");
+	write(1, "\n:", 2);
+	ft_putnbr_base(42, "0");
+	write(1, "\n:", 2);
+	ft_putnbr_base(42, "+-0123456789abcdef");
+	write(1, "\n:", 2);
+	ft_putnbr_base(42, "\t0123456789abcdef");
 }
