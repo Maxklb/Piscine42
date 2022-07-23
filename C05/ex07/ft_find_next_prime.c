3873f999ed77e0bc7129caba9c221053b9ce3a88
Maximilien KOCH LE BRETON <themaxklb@gmail.com>
2022-07-23 16:16:12 +0200

    test

--- C05/ex07/ft_find_next_prime.c
+++ C05/ex07/ft_find_next_prime.c
@@ -6,11 +6,38 @@
 /*   By: mkoch-le <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/21 13:38:25 by mkoch-le          #+#    #+#             */
-/*   Updated: 2022/07/22 14:14:11 by mkoch-le         ###   ########.fr       */
+/*   Updated: 2022/07/23 16:16:01 by mkoch-le         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+int	ft_is_prime(int nb)
+{
+	unsigned int	i;
+
+	i = 2;
+	if (nb < 2)
+		return (0);
+	while (i * i < nb)
+	{
+		if (nb % i == 0)
+			return (0);
+		i++;
+	}
+	return (1);	
+}
+
 int	ft_find_next_prime(int nb)
 {
-	if (nb :wq
-	
+	ft_is_prime(nb);
+	if (nb == 1)
+		return (nb);
+	if (nb == 0)	
+	{
+		while(nb != 1 )
+		{
+			nb++;
+		}
+	}
+}
