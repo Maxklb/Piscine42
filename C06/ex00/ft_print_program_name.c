084fe217130e31a680e41e3985fe61b80f7d23d9
Maximilien KOCH LE BRETON <themaxklb@gmail.com>
2022-07-24 14:53:42 +0200

    test

--- C06/ex00/ft_print_program_name.c
+++ C06/ex00/ft_print_program_name.c
@@ -6,7 +6,7 @@
 /*   By: mkoch-le <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 12:16:55 by mkoch-le          #+#    #+#             */
-/*   Updated: 2022/07/24 14:48:51 by mkoch-le         ###   ########.fr       */
+/*   Updated: 2022/07/24 14:53:21 by mkoch-le         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,5 @@ int	main(int argc, char *argv[])
 {
 	ft_putchar(argv[0]);
 	if (argc < 2)
-		return(0);
+		return (0);
 }
