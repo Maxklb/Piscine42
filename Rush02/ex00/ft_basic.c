9441d314a45377bfafdf366529de9e137e6e7128
Maximilien KOCH LE BRETON <themaxklb@gmail.com>
2022-07-24 17:46:38 +0200

    Minor fixes

--- Rush02/ex00/ft_basic.c
+++ Rush02/ex00/ft_basic.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_basic.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mkoch-le <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/24 17:39:53 by mkoch-le          #+#    #+#             */
-/*   Updated: 2022/07/24 17:40:05 by mkoch-le         ###   ########.fr       */
+/*   Updated: 2022/07/24 17:46:18 by mkoch-le         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,33 @@ int	ft_strcmp(char *s1, char *s2)
 		s2++;
 	}
 	return (*s1 - *s2);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	ft_strlen(char *str)
+{
+	int	a;
+
+	a = 0;
+	while (str[a] != '\0')
+	{
+		a++;
+	}
+	return (a);
+}
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
 }
