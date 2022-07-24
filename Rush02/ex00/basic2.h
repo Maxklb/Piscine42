772f413d758dfa82c41b79294f7114894560f82e
Maximilien KOCH LE BRETON <themaxklb@gmail.com>
2022-07-24 19:44:10 +0200

    Fix norminette

--- Rush02/ex00/basic2.h
+++ Rush02/ex00/basic2.h
@@ -6,19 +6,19 @@
 /*   By: mkoch-le <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/24 19:09:02 by mkoch-le          #+#    #+#             */
-/*   Updated: 2022/07/24 19:12:10 by mkoch-le         ###   ########.fr       */
+/*   Updated: 2022/07/24 19:41:12 by mkoch-le         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BASIC2_H
-#define BASIC2_H
+# define BASIC2_H
 
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdbool.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdbool.h>
 
-int	ft_atoi(char *str);
-void ft_div_mod(int a, int b, int *div, int *mod);
+int		ft_atoi(char *str);
+void	ft_div_mod(int a, int b, int *div, int *mod);
 
 #endif
--- Rush02/ex00/ft_check_input_args.c
+++ Rush02/ex00/ft_check_input_args.c
@@ -6,18 +6,18 @@
 /*   By: mkoch-le <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/24 19:15:01 by mkoch-le          #+#    #+#             */
-/*   Updated: 2022/07/24 19:15:04 by mkoch-le         ###   ########.fr       */
+/*   Updated: 2022/07/24 19:38:18 by mkoch-le         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+#include "unistd.h"
 
-int ft_check_input_args(int ac, char **av, int *input)
+int	ft_check_input_args(int ac, char **av, int *input)
 {
-  if ((ac < 2 && ac > 3)|| (*input = ft_atoi(av)) < 0)
-  {
-	  write(1, "error", 5);
-	  return (-1);
-  }
-  return (*input);
+	if ((ac < 2 && ac > 3) || (*input = ft_atoi(av)) < 0)
+	{
+		write(1, "error", 5);
+		return (-1);
+	}
+	return (*input);
 }
