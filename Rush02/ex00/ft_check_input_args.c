a240799e504dc05323a38e49721415c05c51616a
Maximilien KOCH LE BRETON <themaxklb@gmail.com>
2022-07-24 20:53:02 +0200

    Nothing

--- Rush02/ex00/ft_check_input_args.c
+++ Rush02/ex00/ft_check_input_args.c
@@ -6,15 +6,18 @@
 /*   By: mkoch-le <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/24 19:15:01 by mkoch-le          #+#    #+#             */
-/*   Updated: 2022/07/24 19:57:55 by mkoch-le         ###   ########.fr       */
+/*   Updated: 2022/07/24 20:52:30 by mkoch-le         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "unistd.h"
+#include <unistd.h>
+#include "basic.h"
+#include "basic2.h"
 
 int	ft_check_input_args(int ac, char **av, int *input)
 {
-	if ((ac < 2 && ac > 3) || *input = ft_atoi(av) < 0)
+	*input = (ft_atoi(av));
+	if ((ac < 2 && ac > 3) || (*input < 0))
 	{
 		write(1, "error", 5);
 		return (-1);
