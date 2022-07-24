edf89d86d521ce051c35b30235896539c4e82a05
Maximilien KOCH LE BRETON <themaxklb@gmail.com>
2022-07-24 18:44:47 +0200

    Add subject

--- Rush02/ex00/basic.h
+++ Rush02/ex00/basic.h
@@ -6,16 +6,21 @@
 #    By: mkoch-le <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/07/24 18:05:02 by mkoch-le          #+#    #+#              #
-/*   Updated: 2022/07/24 18:09:41 by mkoch-le         ###   ########.fr       */
+/*   Updated: 2022/07/24 18:42:35 by mkoch-le         ###   ########.fr       */
 #                                                                              #
 # **************************************************************************** #
 
-#ifndef FT_BASIC_H
+#ifndef BASIC_H
 #define BASIC_H
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdbool.h>
+
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
 
 #endif
