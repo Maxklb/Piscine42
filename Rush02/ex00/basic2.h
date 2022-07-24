f4f8b0cb46c62eadbd6b91180f864d82ab277083
Maximilien KOCH LE BRETON <themaxklb@gmail.com>
2022-07-24 19:12:21 +0200

    Minor add

---
+++ Rush02/ex00/basic2.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   basic2.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkoch-le <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/24 19:09:02 by mkoch-le          #+#    #+#             */
+/*   Updated: 2022/07/24 19:12:10 by mkoch-le         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BASIC2_H
+#define BASIC2_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int	ft_atoi(char *str);
+void ft_div_mod(int a, int b, int *div, int *mod);
+
+#endif
