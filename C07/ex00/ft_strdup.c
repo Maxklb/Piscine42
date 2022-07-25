64a7f34d7f4f79f07e2ffa18c56245c838e0b6dc
Maximilien KOCH LE BRETON <themaxklb@gmail.com>
2022-07-25 16:22:32 +0200

    Add ft_strdup

--- C07/ex00/ft_strdup.c
+++ C07/ex00/ft_strdup.c
@@ -6,10 +6,43 @@
 /*   By: mkoch-le <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/23 19:40:40 by mkoch-le          #+#    #+#             */
-/*   Updated: 2022/07/23 19:41:03 by mkoch-le         ###   ########.fr       */
+/*   Updated: 2022/07/25 16:20:39 by mkoch-le         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+
+char	ft_strlen(char *str)
+{
+	int	index;
+
+	index = 0;
+	while (str[index] != '\0')
+		index++;
+	return (index);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	index;
+
+	index = 0;
+	while (src[index] != '\0')
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	dest[index] = '\0';
+	return (dest);
+}
+
 char	*ft_strdup(char *src)
 {
-	
+	char	*dest;
+
+	dest = malloc((ft_strlen(src) + 1) * sizeof(char));
+	if (dest == NULL)
+		return (NULL);
+	ft_strcpy(dest, src);
+	return (dest);
+}
