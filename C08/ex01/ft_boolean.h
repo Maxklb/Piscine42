197b7987b5cba236d745a914cb6a5519ec04d58f
Maximilien KOCH LE BRETON <themaxklb@gmail.com>
2022-07-25 13:14:13 +0200

    Add ft_boolean.h

--- C08/ex01/ft_boolean.h
+++ C08/ex01/ft_boolean.h
@@ -1,24 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft.h                                               :+:      :+:    :+:   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mkoch-le <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/25 12:53:50 by mkoch-le          #+#    #+#             */
-/*   Updated: 2022/07/25 13:04:22 by mkoch-le         ###   ########.fr       */
+/*   Created: 2022/07/25 13:05:25 by mkoch-le          #+#    #+#             */
+/*   Updated: 2022/07/25 13:13:59 by mkoch-le         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_H
-# define FT_H
+#ifndef FT_BOOLEAN_H
+# define FT_BOOLEAN_H
 
 # include <unistd.h>
 
-void	ft_putchar(char c);
-void	ft_swap(int *a, int *b);
-void	ft_putstr(char *str);
-int		ft_strlen(char *str);
-int		ft_strcmp(char *s1, char *s2);
+typedef char t_bool;
+# define TRUE 1
+# define FALSE 0
+
+# define SUCCESS 0
+# define EVEN_MSG "I have an even number of arguments.\n"
+# define ODD_MSG "I have an odd number of arguments.\n"
 
 #endif
