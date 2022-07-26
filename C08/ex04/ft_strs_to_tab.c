532800e2a9d831e7d8bc9eb9e852be17a3e9a068
Maxklb <themaxklb@gmail.com>
2022-07-26 22:48:46 +0200

    Save for the nouit

---
+++ C08/ex04/ft_strs_to_tab.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maxklb <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/26 22:22:48 by maxklb            #+#    #+#             */
+/*   Updated: 2022/07/26 22:48:13 by maxklb           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "ft_stock_str.h"
+
+int		ft_strlen(char *str)
+{
+	char	index;
+	
+	index = 0;
+	while (str[index])
+		index++;
+	return (index);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int		index;
+
+	index = 0;
+	while (src[index])
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	dest[index] = '\0';
+	return (dest);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+
+	dest = malloc((ft_strlen(src) + 1) * sizeof(char));
+	if (dest == NULL)
+		return (NULL);
+	ft_strcpy(dest, src);
+	return (dest);
+}
+
+struct	s_stock_str	*ft_strs_to_tab(int argc, char **argv)
+{
+
+}
