dd169ac87e02e16545a0bccf2e954701673ac63d
Maxklb <themaxklb@gmail.com>
2022-07-26 18:28:24 +0200

    Add ft_range

--- C07/ex01/ft_range.c
+++ C07/ex01/ft_range.c
@@ -6,7 +6,7 @@
 /*   By: mkoch-le <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 16:36:32 by mkoch-le          #+#    #+#             */
-/*   Updated: 2022/07/26 15:23:07 by mkoch-le         ###   ########.fr       */
+/*   Updated: 2022/07/26 18:28:14 by maxklb           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,20 @@
 
 int	*ft_range(int min, int max)
 {
+	int	*temp;
+	int	index;
+
 	if (min >= max)
 		return (NULL);
-	
+	max = max - min;
+	temp = malloc(max * sizeof(int));
+	if (temp == NULL)
+		return (NULL);
+	index = 0;
+	while (index < max)
+	{
+		temp[index] = min + index;
+		index++;
+	}
+	return (temp);
+}
