07885d2f4f217e912fe0d2e89b86a93fd8d3cf44
Maximilien KOCH LE BRETON <themaxklb@gmail.com>
2022-07-27 09:33:31 +0200

    Save

---
+++ C08/ex05/ft_show_tab.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_show_tab.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkoch-le <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/27 09:18:37 by mkoch-le          #+#    #+#             */
+/*   Updated: 2022/07/27 09:33:21 by mkoch-le         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include "ft_stock_str.h"
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+}
+
+void	ft_show_tab(struct s_stock_str *par)
+{
+	
+}
