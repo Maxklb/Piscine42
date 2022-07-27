b0db17a85035847496b5e64c100fe89da03c6a5c
Maximilien KOCH LE BRETON <themaxklb@gmail.com>
2022-07-27 12:49:51 +0200

    oopf

--- C08/ex01/ft_boolean.h
+++ C08/ex01/ft_boolean.h
@@ -6,7 +6,7 @@
 /*   By: mkoch-le <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/25 13:05:25 by mkoch-le          #+#    #+#             */
-/*   Updated: 2022/07/27 12:41:09 by mkoch-le         ###   ########.fr       */
+/*   Updated: 2022/07/27 12:46:11 by mkoch-le         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,7 @@ typedef char	t_bool;
 # define SUCCESS 0
 # define EVEN_MSG "I have an even number of arguments.\n"
 # define ODD_MSG "I have an odd number of arguments.\n"
+
+# define EVEN(X) ((X % 2) ? 0 : 1)
 
 #endif
